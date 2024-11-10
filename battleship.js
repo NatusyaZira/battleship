ab502528035c8522a42c1e9b040d5bacc4dd9962
Nataliia Kovalova <natusya.zira@outlook.com>
2024-11-10 20:07:35 +0200

    Create a model

--- battleship.js
+++ battleship.js
@@ -1,31 +1,36 @@
-let randomLoc = Math.floor(Math.random() * 5)
-let location1 = randomLoc;
-let location2 = location1 + 1;
-let location3 = location2 + 1;
-let guess;
-let hits = 0;
-let guesses = 0;
-let isSunk = false;
-
-while(isSunk == false){
-  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-  if(guess < 0 || guess > 6){
-    alert("Please enter a valid cell number!");
-  }
-  else{
-    guesses = guesses + 1;
-
-    if(guess == location1 || guess == location2|| guess == location3){
-      alert("HIT!");
-      hits = hits + 1;
-      if(hits == 3){
-        isSunk = true;
-        alert("You sank my battleship!");
+let model = {
+  boardSize: 7,
+  numShipe: 3,
+  shipLength: 3,
+  shipSunk: 0,
+  ships: [{locations: ['06', '16', '26'], hits: ['', '', '']},
+           {locations: ['24', '34', '44'], hits: ['', '', '']},
+           {locations: ['10', '11', '12'], hits: ['', '', '']}],
+  fire: function(guess){
+    for(let i = 0; i < this.numships; i++){
+      let ship = this.ships[i];
+      let locations = ships.locations;
+      let index = locations.indexOf[guess];
+      if(index >= 0){
+        ship.hits[index] = "hit";
+        return true;
       }
-    } else {
-      alert("MISS");
-    }
+    } return false;
+  }
+};
+let view = {
+  displayMessage: function (msg){
+    let messageArea = document.getElementById('messageArea');
+    messageArea.innerHTML = msg;
+    console.log(msg)
+  },
+  displayHit: function (location){
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location){
+    let cell = document.getElementById(location);
+    cell.setAttribute('class', 'miss');
   }
 }
-let stats = "You took " + guesses + " guesses to sink the battleship, " + "which mins your shooting accuracy was " + (3/guesses);
-alert(stats);
+
